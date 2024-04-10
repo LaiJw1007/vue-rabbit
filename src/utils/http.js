6bf9262b6bc772184aaf132d6ff9b48930301e8f
LaiJw1007 <690575842@qq.com>
2024-04-10 18:52:52 +0800

    jingtai ziyuan yinru and chajian install

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 // axios基础的封装
 import axios from "axios";
 
+// 创建实例
 const httpInstance = axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout:5000
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>我是首页
+    <!-- 二级路由出口 -->
+    <RouterView/>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
