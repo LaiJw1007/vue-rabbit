6c6802be461399eb30243dd1b31a1ae419830574
LaiJw1007 <690575842@qq.com>
2024-04-10 19:30:57 +0800

    layout:get and xuanran yiji nav liebiao

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { getCategoryAPI } from '@/apis/layout';
+import { onMounted,ref } from 'vue';
 
+// 渲染模板
+const categoryList = ref([])
+
+// 接口测试
+const getCategory = async ()=>{
+  // 返回的是promise 所以加await
+  const res = await getCategoryAPI()
+  console.log(res);
+  categoryList.value = res.result  //通过打印查看数组存放在result里
+}
+onMounted(()=>{
+  getCategory()
+})
 </script>
 
 <template>
@@ -9,12 +24,9 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <li class="home" v-for="item in categoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
